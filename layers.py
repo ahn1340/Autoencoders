c578e1026511cdcda4bd06e6e079ab7fa5d91562
unknown <ahn1340@hotmail.com>
2022-08-02 16:04:34 +0900

    add: VQ layer and residual layer for VQ VAE

--- layers.py
+++ layers.py
@@ -1,6 +1,7 @@
 """
 Implementation of ReLU-Conv-BN layers
 """
+import torch
 from torch import nn
 
 
@@ -30,3 +31,34 @@ class ReLUUpConvBN(nn.Module):
 
     def forward(self, x):
         return self.op(x)
+
+
+class VQLayer(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx, zq, emb_dict):
+        N, C, H, W = zq.size()
+        zq = zq.view((N, C, H*W)).transpose(1, 2)  # [N, H*W, C]
+        dist = torch.cdist(zq, emb_dict)  # [N, H*W, C] x [K, C] -> [N, H*W, K]
+        argmins = torch.argmin(dist, dim=2)
+        ze = emb_dict[argmins].transpose(1, 2).view(N, C, H, W)  # construct z_e
+        return ze
+
+    @staticmethod
+    def backward(ctx, grad_outputs):
+        return grad_outputs, None
+
+
+class ResidualLayer(nn.Module):
+    def __init__(self, C_in, C_hidden, C_out):
+        super(ResidualLayer, self).__init__()
+        self.residual_layer = nn.Sequential(
+            nn.ReLU(),
+            nn.Conv2d(C_in, C_hidden, kernel_size=(3, 3), padding=1),
+            nn.ReLU(),
+            nn.Conv2d(C_hidden, C_out, kernel_size=(1, 1), padding=0),
+        )
+
+    def forward(self, x):
+        return x + self.residual_layer(x)
+
+
